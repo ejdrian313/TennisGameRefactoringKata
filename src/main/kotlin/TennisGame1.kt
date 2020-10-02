a243183ca2e06b7ad5bda0beb41f815e17dad132
Adrian Kujawski <adriankujawski313@gmail.com>
2020-10-02 20:28:32 +0200

    rename

--- src/main/kotlin/TennisGame1.kt
+++ src/main/kotlin/TennisGame1.kt
@@ -1,27 +1,27 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private var scoreOne: Int = 0
+    private var scoreTwo: Int = 0
 
     override fun wonPoint(playerName: String) {
         if (playerName === "player1")
-            m_score1 += 1
+            scoreOne += 1
         else
-            m_score2 += 1
+            scoreTwo += 1
     }
 
     override fun getScore(): String {
         var score = ""
         var tempScore = 0
-        if (m_score1 == m_score2) {
-            when (m_score1) {
+        if (scoreOne == scoreTwo) {
+            when (scoreOne) {
                 0 -> score = "Love-All"
                 1 -> score = "Fifteen-All"
                 2 -> score = "Thirty-All"
                 else -> score = "Deuce"
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
+        } else if (scoreOne >= 4 || scoreTwo >= 4) {
+            val minusResult = scoreOne - scoreTwo
             if (minusResult == 1)
                 score = "Advantage player1"
             else if (minusResult == -1)
@@ -33,10 +33,10 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         } else {
             for (i in 1..2) {
                 if (i == 1)
-                    tempScore = m_score1
+                    tempScore = scoreOne
                 else {
                     score += "-"
-                    tempScore = m_score2
+                    tempScore = scoreTwo
                 }
                 when (tempScore) {
                     0 -> score += "Love"
